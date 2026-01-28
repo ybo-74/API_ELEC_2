594b53d46dd0314d2063c3d0a7b8c886e0f0fe88
ybo-74 <User@boymmanuela@gmail.com>
2026-01-28 20:25:26 +0800

    Added Model folder and Employee.cs (get and set)

---
+++ API_ELEC_2/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace API_ELEC_2.Models
+{
+    public class Employee
+    {
+        public int employeeID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string position { get; set; }
+        public DateOnly birthDate { get; set; }
+    }
+}
