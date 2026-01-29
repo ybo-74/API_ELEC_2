b2b8a2ea3ba9ff925f810f1074147dc5f88c59ab
ybo-74 <User@boymmanuela@gmail.com>
2026-01-29 19:32:43 +0800

    updated program.cs to use swagger for testing. openAPI is currently on commented lines: 8 and 17

--- API_ELEC_2/Program.cs
+++ API_ELEC_2/Program.cs
@@ -4,14 +4,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+//builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
