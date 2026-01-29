384ba307a25bf7045d3db773cc73943396757ab6
ybo-74 <User@boymmanuela@gmail.com>
2026-01-29 16:14:59 +0800

    Installed and using Microsoft.Data.SqlClient now instead of System.Data.SqlClient as suggested by intellicode

--- API_ELEC_2/Repositories/EmployeeRepository.cs
+++ API_ELEC_2/Repositories/EmployeeRepository.cs
@@ -1,6 +1,18 @@
-﻿namespace API_ELEC_2.Repositories
+﻿using API_ELEC_2.Models;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace API_ELEC_2.Repositories
 {
     public class EmployeeRepository
     {
+        private readonly string _connectionString;
+        public EmployeeRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+        public List<Employee> GetAllEmployees() {
+        
+        }
     }
 }
