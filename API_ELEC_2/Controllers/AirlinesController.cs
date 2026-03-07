8040c544a68511dc7a13a233b259f9c0f8a042cb
William Kyle <iballa.kylengis@email.com>
2026-03-07 17:57:49 +0800

    Airline Controller

---
+++ API_ELEC_2/Controllers/AirlinesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using API_ELEC_2.Repositories;
+using API_ELEC_2.Models;
+
+namespace API_ELEC_2.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AirlinesController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public AirlinesController(IConfiguration configuration) => _configuration = configuration;
+
+        [HttpGet]
+        public IEnumerable<Airline> GetAirlines()
+        {
+            var repo = new AirlineRepository(_configuration);
+            return repo.GetAllAirlines();
+        }
+    }
+}
