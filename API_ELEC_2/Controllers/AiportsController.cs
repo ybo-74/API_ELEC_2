8e15df5e15b07d61becb971b00c4ada38344a6c1
William Kyle <iballa.kylengis@email.com>
2026-03-07 18:06:49 +0800

    Added Flight Details Controller

---
+++ API_ELEC_2/Controllers/AiportsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using API_ELEC_2.Repositories;
+using API_ELEC_2.Models;
+
+namespace API_ELEC_2.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AirportsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public AirportsController(IConfiguration configuration) => _configuration = configuration;
+
+        [HttpGet]
+        public IEnumerable<Airport> GetAirports()
+        {
+            var repo = new AirportRepository(_configuration);
+            return repo.GetAllAirports();
+        }
+    }
+}
+using Microsoft.AspNetCore.Mvc;
+using API_ELEC_2.Repositories;
+using API_ELEC_2.Models;
+
+namespace API_ELEC_2.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AirportsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public AirportsController(IConfiguration configuration) => _configuration = configuration;
+
+        [HttpGet]
+        public IEnumerable<Airport> GetAirports()
+        {
+            var repo = new AirportRepository(_configuration);
+            return repo.GetAllAirports();
+        }
+    }
+}
